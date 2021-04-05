64807969cfb90098dbe783123ce19361e214245e
Michael <m.muenz@gmail.com>
2021-04-05 07:42:04 +0200

    rename plugin

---
+++ Templates/actions.d
@@ -0,0 +1,1 @@
+if [ -f /usr/local/etc/rc.d/configd ]; then /usr/local/etc/rc.d/configd restart; fi
---
+++ Templates/rc.loader.d
@@ -0,0 +1,1 @@
+if [ -f /usr/local/etc/rc.configure_firmware ]; then echo "Reloading firmware configuration"; /usr/local/etc/rc.configure_firmware; fi
