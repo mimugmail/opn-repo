8ffbd0acfc593fce9b2cc9696964a03997c93a5b
Michael <m.muenz@gmail.com>
2021-04-08 06:42:18 +0200

    Update speedtest.widget.php
    Closes #30

--- net-mgmt/speedtest-mihakralj/src/www/widgets/widgets/speedtest.widget.php
+++ net-mgmt/speedtest-mihakralj/src/www/widgets/widgets/speedtest.widget.php
@@ -25,27 +25,20 @@
  * POSSIBILITY OF SUCH DAMAGE.
  */
 require_once("guiconfig.inc");
-$command = escapeshellcmd('/usr/local/opnsense/scripts/OPNsense/speedtest/opn_speedtest.py stat');
-$output = shell_exec($command);
-$obj = json_decode($output, True);
 ?>
+<script>
+    $(document).ready(function() {
+        ajaxGet("/api/speedtest/service/showstat", {}, function(l,status) {
+            $('#stat_samples').html("<b>"+l.samples+"<\/b>")
+            $('#stat_latency').html("<b>"+l.latency.avg+" ms<\/b><small> (min: "+l.latency.min+" ms, max: "+l.latency.max +" ms)</small>")
+            $('#stat_download').html("<b>"+l.download.avg+" Mbps<\/b><small> (min: "+l.download.min+" Mbps, max: "+l.download.max +" Mbps)</small>")
+            $('#stat_upload').html("<b>"+l.upload.avg+" Mbps<\/b><small> (min: "+l.upload.min+" Mbps, max: "+l.upload.max +" Mbps)</small>")
+        });
+    });
+</script>
 <!-- gateway table -->
 <table id="speedtest_widget_table" class="table table-striped table-condensed">
-  <tr>
-      <td>
-          Avg Latency:
-      </td>
-      <td><div id="stat_latency"><?php echo $obj["latency"]["avg"]?> ms (min: <?php echo $obj["latency"]["min"]?> ms, max: <?php echo $obj["latency"]["max"]?> ms)</div>
-      </td>
-  </tr>
-  <tr>
-      <td>Avg Download:</td>
-      <td><div id="stat_download"><?php echo $obj["download"]["avg"]?> Mbps (min: <?php echo $obj["download"]["min"]?> Mbps, max: <?php echo $obj["download"]["max"]?> Mbps)</div>
-      </td>
-  </tr>
-  <tr>
-      <td>Avg Upload:</td>
-      <td><div id="stat_upload"><?php echo $obj["upload"]["avg"]?> Mbps (min: <?php echo $obj["upload"]["min"]?> Mbps, max: <?php echo $obj["upload"]["max"]?> Mbps)</div>
-        </td>
-  </tr>
+  <tr><td style="width:25%">Avg Latency:</td><td><div id="stat_latency">0.00 ms (min: 0.00 ms, max: 0.00 ms)</div></td></tr>
+  <tr><td>Avg Download:</td><td><div id="stat_download">0 Mbps (min: 0 Mbps, max: 0 Mbps)</div></td></tr>
+  <tr><td>Avg Upload:</td><td><div id="stat_upload">0 Mbps (min: 0 Mbps, max: 0 Mbps)</div></td></tr>
 </table>
