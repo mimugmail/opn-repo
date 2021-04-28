17f1dbecc27bf0c79302b89f39a1f1fab49a535f
mihakralj <miha@kraljfamily.net>
2021-04-27 22:54:45 -0700

    code optimized for speed

--- misc/theme-solarized/src/opnsense/www/themes/solarized/build/js/theme.js
+++ misc/theme-solarized/src/opnsense/www/themes/solarized/build/js/theme.js
@@ -1,22 +1,17 @@
 document.addEventListener('readystatechange', (event) => {
-    //const theme = localStorage.getItem('theme') ? localStorage.getItem('theme') : 'light';
 	document.documentElement.setAttribute('data-theme', getTheme());
 });
 
 $(document).ready(function() {
 	$('ul.nav.navbar-nav.navbar-right').append("<li><span class=\"navbar-theme\" id=\"theme_toggle\"><label class=\"theme-switch\" for=\"checkbox\"><input type=\"checkbox\" id=\"checkbox\" /><div class=\"slider round\"></div></label></span>")
-
 	const toggleSwitch = document.querySelector('.theme-switch input[type="checkbox"]');
 	toggleSwitch.addEventListener('input', switchTheme);
-
 	if (getTheme() === 'dark') {toggleSwitch.checked = true;}
 });
 
 function getTheme() {
-	var theme = 'light';
-	if (localStorage.getItem('theme')) {
-		if (localStorage.getItem('theme') === 'dark') {theme = 'dark';}
-	} else if (window.matchMedia('(prefers-color-scheme: dark)').matches) {theme = 'dark';}
+	if (localStorage.getItem('theme')) {theme = localStorage.getItem('theme');}
+	else theme = (window.matchMedia('(prefers-color-scheme: dark)').matches) ? 'dark' : 'light';
 	return theme;
 }
 
