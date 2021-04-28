cc62f75d15e6c084c9b2b5171aca7ccccbdb9571
mihakralj <miha@kraljfamily.net>
2021-04-27 22:34:57 -0700

    faster triggering of css, follows OS dark/light theme

--- misc/theme-solarized/src/opnsense/www/themes/solarized/build/js/theme.js
+++ misc/theme-solarized/src/opnsense/www/themes/solarized/build/js/theme.js
@@ -1,18 +1,24 @@
-$( document ).ready(function() {
+document.addEventListener('readystatechange', (event) => {
+    //const theme = localStorage.getItem('theme') ? localStorage.getItem('theme') : 'light';
+	document.documentElement.setAttribute('data-theme', getTheme());
+});
+
+$(document).ready(function() {
 	$('ul.nav.navbar-nav.navbar-right').append("<li><span class=\"navbar-theme\" id=\"theme_toggle\"><label class=\"theme-switch\" for=\"checkbox\"><input type=\"checkbox\" id=\"checkbox\" /><div class=\"slider round\"></div></label></span>")
 
-const toggleSwitch = document.querySelector('.theme-switch input[type="checkbox"]');
-toggleSwitch.addEventListener('input', switchTheme);
-const currentTheme = localStorage.getItem('theme') ? localStorage.getItem('theme') : null;
-if (currentTheme) {
-	document.documentElement.setAttribute('data-theme', currentTheme);
-	if (currentTheme === 'dark') {
-		toggleSwitch.checked = true;
-	}
-}
+	const toggleSwitch = document.querySelector('.theme-switch input[type="checkbox"]');
+	toggleSwitch.addEventListener('input', switchTheme);
 
-$('#debug').text("storage: "+currentTheme);
+	if (getTheme() === 'dark') {toggleSwitch.checked = true;}
 });
+
+function getTheme() {
+	var theme = 'light';
+	if (localStorage.getItem('theme')) {
+		if (localStorage.getItem('theme') === 'dark') {theme = 'dark';}
+	} else if (window.matchMedia('(prefers-color-scheme: dark)').matches) {theme = 'dark';}
+	return theme;
+}
 
 function switchTheme(e) {
 	if (e.target.checked) {
@@ -24,9 +30,3 @@ function switchTheme(e) {
 		localStorage.setItem('theme', 'light');
 	}
   };
-
-
-  /*
-              <li><span class="navbar-text" id="theme_toggle"><label class="theme-switch" for="checkbox"><input type="checkbox" id="checkbox" /></label></span>
-            </li>
-  */
