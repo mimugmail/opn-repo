114e4053423f433a430715b5509ef24e9340e4e1
mihakralj <miha@kraljfamily.net>
2021-05-05 17:40:53 -0700

    New icons, new font, new happyness

--- misc/theme-solarized/src/opnsense/www/themes/solarized/build/js/theme.js
+++ misc/theme-solarized/src/opnsense/www/themes/solarized/build/js/theme.js
@@ -1,8 +1,21 @@
 document.addEventListener('readystatechange', (event) => {
+	document.styleSheets[4].disabled = true;
 	document.documentElement.setAttribute('data-theme', getTheme());
 });
 
 $(document).ready(function() {
+
+	$('.fa-building-o').addClass('fa-microchip').removeClass('fa-building-o');
+	$('.fa-map-signs').addClass('fa-network-wired').removeClass('fa-map-signs');
+	$('.fa-map').addClass('fa-chart-network').removeClass('fa-map');
+	$('.fa-expand').addClass('fa-network-wired').removeClass('fa-expand');
+	$('.fa-lock').addClass('fa-shield-check').removeClass('fa-lock');
+	$('.fa-clock-o').addClass('fa-clock').removeClass('fa-clocl-o');
+	$('.fa-refresh').addClass('fa-redo').removeClass('fa-refresh');
+	$('.glyphicon.glyphicon-fire').addClass('fa fa-route-highway').removeClass('glyphicon glyphicon-fire');
+	$('.fa-support').addClass('fa fa-question-circle').removeClass('fa-support');
+	$('.fa').addClass('fal fa-lg').removeClass('fa');
+
 	$('ul.nav.navbar-nav.navbar-right').append("<li><span class=\"navbar-theme\" id=\"theme_toggle\"><label class=\"theme-switch\" for=\"checkbox\"><input type=\"checkbox\" id=\"checkbox\" /><div class=\"slider round\"></div></label></span>")
 	const toggleSwitch = document.querySelector('.theme-switch input[type="checkbox"]');
 	toggleSwitch.addEventListener('input', switchTheme);
@@ -25,3 +38,5 @@ function switchTheme(e) {
 		localStorage.setItem('theme', 'light');
 	}
   };
+
+  
