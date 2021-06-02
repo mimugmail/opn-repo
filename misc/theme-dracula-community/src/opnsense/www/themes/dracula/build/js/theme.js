3f5a95b39af01529791671151fe658633090581b
Russell <rpoovey@icloud.com>
2021-06-02 10:00:18 -0400

    Corrected displaying of some icons

--- misc/theme-dracula-community/src/opnsense/www/themes/dracula/build/js/theme.js
+++ misc/theme-dracula-community/src/opnsense/www/themes/dracula/build/js/theme.js
@@ -1,5 +1,11 @@
 $(document).ready(function() {
 
+	$('.fa-paper-plane-o').addClass('fa-paper-plane-o').removeClass('fa-paper-plane-o');
+	$('.fa-check-square-o').addClass('fa-check-square').removeClass('fa-check-square-o');
+	$('.fa-plus-square-o').addClass('fa-plus-square-o').removeClass('fa-plus-square-o');
+	$('.fa-folder-o').addClass('fa-folder-o').removeClass('fa-folder-o');
+	$('.fa-square-o').addClass('fa-square').removeClass('fa-square-o');
+	$('.fa-trash').addClass('fa-trash').removeClass('fa-trash');
 	$('.fa-building-o').addClass('fa-microchip').removeClass('fa-building-o');
 	$('.fa-map-signs').addClass('fa-network-wired').removeClass('fa-map-signs');
 	$('.fa-map').addClass('fa-chart-network').removeClass('fa-map');
@@ -10,6 +16,5 @@ $(document).ready(function() {
 	$('.glyphicon.glyphicon-fire').addClass('fa fa-route-highway').removeClass('glyphicon glyphicon-fire');
 	$('.fa-support').addClass('fa fa-question-circle').removeClass('fa-support');
 	$('.fa').addClass('fal fa-lg').removeClass('fa');
-
 });
   
